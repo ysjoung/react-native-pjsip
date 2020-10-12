bef10982b3e8b69d9cb546c6418d22f541e27240
ryanmcclure4 <ryanmcclure4@gmail.com>
2020-10-12 16:30:59 -0700

    fixes for ios

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,14 +1,12 @@
 import { default as Account } from './Account';
 import { default as Call } from './Call';
 import { default as Endpoint } from './Endpoint';
-import { default as PreviewVideoView } from './PreviewVideoView';
-import { default as RemoteVideoView } from './RemoteVideoView';
 export * from './Account';
 export * from './Call';
 export * from './Endpoint';
 export * from './PreviewVideoView';
 export * from './RemoteVideoView';
-export { Account, Call, Endpoint, PreviewVideoView, RemoteVideoView, };
+export { Account, Call, Endpoint, };
 declare const _default: {
     Account: typeof Account;
     Call: typeof Call;
